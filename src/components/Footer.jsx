d1dd896e3d7dc6050c2d47aef7a4c6498d83c50d
skumar <ch@ng3m3>
2023-02-25 16:03:00 -0600

    FCC React: Team member allocation, header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
-
 const Footer = () => {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <p style={{ textAlign: "center" }}>
+            Team Memeber Allocation App &copy; {new Date().getFullYear()}
+          </p>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-const Header = () => {
+const Header = ({ selectedTeam, numberOfEmployees }) => {
   return (
-    <header>
-     <h1>Header</h1> 
+    <header className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h1>Team Memeber Allocation</h1>
+          <h2>
+            {selectedTeam} has {numberOfEmployees} members.
+          </h2>
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
